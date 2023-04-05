837ff8dd1a214377a64837ae1b426989191b2cdd
odaney baker <bakerobtb@gmail.com>
2023-04-05 19:54:07 -0400

    Done

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const batteries = (state = { totalBatteries: 0 }, action) => {
+    switch (action.type) {
+      case 'ADD_BATTERIES':
+        return { totalBatteries: state.totalBatteries + action.payload };
+      default:
+        return state;
+    }
+  };
+  
+  const totalBatteries = batteryBatches.reduce((state, batch) => {
+    return batteries(state, { type: 'ADD_BATTERIES', payload: batch });
+  }, { totalBatteries: 0 }).totalBatteries;
+  
+  console.log(totalBatteries);
+  
